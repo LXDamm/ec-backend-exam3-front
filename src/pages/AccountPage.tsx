0984434e712d53287944f857b91b3447ad18b8b7
Lexx Damm <lexx.damm@gmail.com>
2021-06-13 14:07:03 +0200

    for rebecca

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -1,7 +1,17 @@
-function AccountPage() {
+import { useSelector } from "react-redux";
+import { useAppSelector } from "../redux/hooks";
+import { AppState } from "../redux/store";
+import { Account } from "../types/account";
 
+function AccountPage() {
+    const account: Account = useSelector((state: AppState) => {
+        return state.account;
+    });
     return (
-        <></>
+        <div className="AccountPage">
+            <p>Work!</p>
+            <p>{account.username}</p>
+        </div>
     );
 }
 
